6217eabb974b208c547d6033945bbf7f5b83c198
Nathan Totten <ntotten@salesforce.com>
2018-10-30 14:44:33 -0400

    enhancements to project-def schema

---
+++ project-scratch-def.json/project-scratch-def.test.js
@@ -0,0 +1,32 @@
+const schema = require("./project-scratch-def.schema.json");
+const Ajv = require("ajv");
+
+const ajv = new Ajv();
+var validate = ajv.compile(schema);
+
+function testFile(path, shouldValidate) {
+  return async () => {
+    let data = require(path);
+    let result = validate(data);
+    expect(result === shouldValidate);
+  };
+}
+
+describe("project-stract-def.json", () => {
+  test(
+    "minimum configuration is valid",
+    testFile("./examples/simple.json", true)
+  );
+  test(
+    "simple configuration is valid",
+    testFile("./examples/simple.json", true)
+  );
+  test(
+    "deprecated orgPreferences option is valid",
+    testFile("./examples/orgPrefs.json", true)
+  );
+  test(
+    "deprecated orgPreferences option with settings is invalid",
+    testFile("./examples/orgPrefs-with-settings-invalid.json", false)
+  );
+});
