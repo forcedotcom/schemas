ea44f3e94440f8a571c7f680ea547dc452fc8e1d
Ricardo Viera <ricardo.viera@salesforce.com>
2020-04-01 16:49:36 -0600

    Added complex package.json file and test for file

---
+++ sfdx-project/sfdx-project.test.js
@@ -0,0 +1,37 @@
+const schema = require("./sfdx-project.schema.json");
+const Ajv = require("ajv");
+
+const ajv = new Ajv();
+var validate = ajv.compile(schema);
+
+function testFile(path, shouldValidate) {
+  console.log(path);
+  console.log(shouldValidate);
+
+  return async () => {
+    let data = require(path);
+    let result = await validate(data);
+
+    expect(result).toBe(shouldValidate);
+  };
+}
+
+describe("sfdx-project.json", () => {
+  test("minimum configuration is valid", testFile("./examples/min.json", true));
+  test(
+    "default configuration is valid",
+    testFile("./examples/default.json", true)
+  );
+  test(
+    "package config is valid",
+    testFile("./examples/package-basic.json", true)
+  );
+  test(
+    "complex package config is valid",
+    testFile("./examples/package-complex.json", true)
+  );
+  test(
+    "package without versionNumber is invalid",
+    testFile("./examples/package-no-versionNumber-invalid.json", false)
+  );
+});
